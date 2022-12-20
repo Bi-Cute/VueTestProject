f4c046e9b2b2ec25736fcbd014d314fce1163e8e
BiCute <bicute@naver.com>
2022-12-21 08:45:25 +0900

    3.2

---
+++ src/views/Test5View.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        더하기
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                data: ['1', '3', '5', '7', '9']
+            }
+        },
+        mounted(){
+            this.reduce()
+            this.forOf()
+            this.for()
+        },
+        methods: {
+            reduce(){
+                const sum = this.data.reduce((acc, element) =>
+                    acc + parseInt(element), 0);
+                console.log('reduce() = ', sum)
+            },
+            forOf(){
+                let sum = 0;
+                for (const element of this.data) {
+                    sum += parseInt(element);
+                }
+                console.log('forOf() = ', sum)
+            },
+            for(){
+                let sum = 0;
+                for (let i = 0; i < this.data.length; i++) {
+                    sum += parseInt(this.data[i]);
+                }
+                console.log('for() = ', sum)
+            }
+        },
+    }
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
